778feb3bfb7726ac9b766cfa05eba88ba88588f4
Arhan Ansari <arhanansari2009@gmail.com>
2024-05-26 08:41:07 +0000

    Initial

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Messeger Tutorial',
+  title: 'Messeger',
   description: 'Send instant messeges!',
 }
 
